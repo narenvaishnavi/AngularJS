b7a9a284876c3a62dc6be98cefe91a6c445f8c22
Naren Vaishnavi <narenvaishnavi@users.noreply.github.com>
2017-07-21 01:18:09 -0400

    Update $http.js

--- $http.js
+++ $http.js
@@ -15,32 +15,8 @@ app.controller('tablecontroller', function($scope, $http,$log) {
             url : "https://www.w3schools.com/angular/customers.php"
           }).then(function sucessHandler(response) {
               $scope.info = response.data.records;
-              $log.info(response);
+              $log.info(response); // object in console
             }, function errorHandler(response) {
               $scope.myWelcome = response.statusText;
           });
-
-      var employees=[
-        {name:"Tom",gender:"1",National_ID: "131",likes:0,Dislikes:0},
-        {name:"Ruby",gender:"2",National_ID: "135",likes:0,Dislikes:0},
-        {name:"Pamela",gender:"1",National_ID: "145",likes:0,Dislikes:0},
-        {name:"Sam",gender:"1",National_ID: "152",likes:0,Dislikes:0},
-        {name:"Moseph",gender:"3",National_ID: "157",likes:0,Dislikes:0},
-        
-        
-    ];
-                     
-        $scope.employees = employees;
-        $scope.rowlimit =3;
-        $scope.message ="Welcome to angularJS";
-                    
-        $scope.like = function(employees){
-           employees.likes++;
-        }
-   
-         $scope.Dislike = function(employees){
-            employees.Dislikes++;
-        }
-   
-
 });
